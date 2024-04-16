57c78b582df1d9290cc1905bceee52743be60227
Asia Derinaldis <derinaldisasia6@gmail.com>
2024-04-16 17:36:51 +0200

    aiutooo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,10 +8,10 @@
                 <!-- inseriamo il logo -->
                 <img class="image" src="/img/logo_presto.jpg" alt="logo del sito" id="logo">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
                 <!-- inseriamo una nostra icona -->
                 <i id="menu-button" class="fa-solid fa-bars-staggered"></i>
-            </button>
+            </button> --}}
 
    
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -64,8 +64,8 @@
                     @endauth
                     @guest
                 </li>
-                <a class="nav-link" href="{{route('register')}}">registrati</a>
-            </li>
+                {{-- <a class="nav-link" href="{{route('register')}}">registrati</a> --}}
+         
             
             @endguest
             <li class="nav-item dropdown">
@@ -81,31 +81,27 @@
             </li>
             @auth
               {{-- Form di logout con method post --}}
-              <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <li>
-                    <button type="submit" class="btn btn-danger w-100">Logout</button>
-                </li>
-            </form>
+           
             @endauth
             
            
-            <div class="buttonLogout">
+          
+            <div class="buttonDad d-flex justify-content-end align-items-end">
+                <a href="{{route('register')}}" class="glow-on-hover" type="button">Registrati</a>
+                <a href="{{route('login')}}" class="glow-on-hover" type="button">Accedi</a>
+            
+           
                 
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     
-                        <button class="glow-on-hover" type="button">HOVER ME, THEN CLICK ME!</button>
+                   <button type="submit" class="button display-2 btn btn-outline-primary"><i class="fa-solid fa-right-to-bracket fa-2xs" style="color: #c81933;"></i></button>
                     
                 </form>
                 {{-- <button href="{{route('login')}}" class="fa-solid fa-right-to-bracket"></button> --}}
                 
                 
             </div>
-            <div class="">
-                <button href="{{route('register')}}" class="glow-on-hover" type="button">Registrati</button>
-            </div>   
-            
             
             <!-- <form role="search">
                 <input class="form-control" type="search" placeholder="Search" aria-label="Search">
@@ -118,80 +114,4 @@
 
 
 
-
-
-{{-- 
-    
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-                    </li>
-                    
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('announcements.index')}}">i nostri annunci</a>
-                    </li>
-                    
-                    
-                    @auth
-                    
-                    <li>
-                        {{-- Form di logout con method post --}}
-                        {{-- <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <li>
-                                <button type="submit" class="btn btn-danger w-100">Logout</button>
-                            </li>
-                        </form>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('create')}}">crea un annuncio</a>
-                        </li>
-                        
-                        
-                        
-                        
-                        
-                        <li class="nav-item">
-                            <p class="nav-link">Benvenuto {{ Auth::user()->name }}</p>
-                        </li>
-                        @else
-                        
-                        <li class="nav-item">
-                            <p class="nav-link">Benvenuto ospite </p>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('login')}}">login</a>
-                        </li>
-                        
-                        
-                        @endauth
-                        @guest
-                    </li>
-                    <a class="nav-link" href="{{route('register')}}">registrati</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Le nostre Categorie
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        @foreach($categories as $category)
-                        
-                        <li><a class="dropdown-item" href="{{route('categoryShow', compact('category'))}}">{{$category->name}}</a></li>
-                        @endforeach
-                    </ul>
-                </li>
-                @endguest
-                
-            </nav>
-            
-            
-            --}}
             
